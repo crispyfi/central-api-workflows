f77ce0462cd6e3b917428aa9063c1f2c40baad31
Chris Denham <chris@crispyfi.com>
2026-02-11 21:34:12 +1000

    Created function for profile creation

--- create_wlan/create_wlan.py
+++ create_wlan/create_wlan.py
@@ -6,12 +6,95 @@ from pycentral.profiles import Profiles
 from pycentral.scopes import Scopes
 from pycentral.utils.url_utils import generate_url
 
-# Define the profile assignment variables
-device_persona = "CAMPUS_AP"
-scope_type = "site_collection"
-scope_name = "Lab"
+def create_profile(
+    endpoint,
+    bulk_key,
+    profile_name,
+    config_file,
+    central_conn,
+    env,
+    variables,
+    device_persona,
+    scope_type,
+    scope_name
+    ):
+    """
+    :param endpoint:
+    :param bulk_key:
+    :param profile_name:
+    :param config_file:
+    """
+
+    # Load and render JSON template
+    try:
+        template = env.get_template(config_file)
+        rendered_json = template.render(variables)
+    except TemplateNotFound:
+        print(f"Error: Config file '{config_file}' not found.")
+        return 1
+    except Exception as e:
+        print(f"Error rendering template '{config_file}': {e}")
+        return 1
+
+    # Convert rendered JSON string to dict
+    try:
+        config_dict = json.loads(rendered_json)
+    except json.JSONDecodeError as e:
+        print(f"Error parsing rendered JSON '{config_file}': {e}")
+        return 1
+
+    # Create API endpoint URL
+    url = generate_url(api_endpoint=endpoint)
+
+    # Create Profile
+    print(f"\nCreating {endpoint} profile...\n")
+    create_profile = Profiles.create_profile(
+        bulk_key=bulk_key,
+        path=url,
+        central_conn=central_conn,
+        config_dict=config_dict,
+    )
+
+    print(create_profile)
+
+    # Assign Profile to Scope
+    scopes = Scopes(central_conn)
+    result = scopes.assign_profile_to_scope(
+        profile_name = f"{endpoint}/{profile_name}",
+        profile_persona=device_persona,
+        scope=scope_type,
+        scope_name=scope_name,
+    )
+
+    # Check the result and print appropriate messages
+    if result:
+        print(f"\nSuccessfully assigned profile to {scope_name}\n")
+    else:
+        print(f"\nError in assigning profile\n")
+        exit()
 
 def main():
+
+    # Setup Jinja environment
+    env = Environment(loader=FileSystemLoader("."))
+
+    # Load YAML variables
+    variables_input = "vars.yaml"
+
+    try:
+        with open(variables_input, "r") as yaml_file:
+            variables = yaml.safe_load(yaml_file)
+    except FileNotFoundError:
+        print(f"Error: YAML file '{variables_input}' not found.")
+        return 1
+    except yaml.YAMLError as e:
+        print(f"Error parsing YAML file '{variables_input}': {e}")
+        return 1
+        
+    # Load profile assignment variables
+    device_persona = variables["assignment"]["device_persona"]
+    scope_type = variables["assignment"]["scope_type"]
+    scope_name = variables["assignment"]["scope_name"]
 
     # Load the credentials from the JSON file
     credentials_file = "central_token.json"
@@ -36,75 +119,36 @@ def main():
     except Exception as e:
         print(f"\nFailed to connect to Central: {e}")
         return 1
-
-    # Load configuration file
-    config_file = "wlan-ssid.json"
-    yaml_file = "vars.yaml"
-
-    # Load YAML variables
-    try:
-        with open(yaml_file, "r") as file:
-            variables = yaml.safe_load(file)
-    except FileNotFoundError:
-        print(f"Error: YAML file '{yaml_file}' not found.")
-        return 1
-    except yaml.YAMLError as e:
-        print(f"Error parsing YAML file '{yaml_file}': {e}")
-        return 1
-
-    # Setup Jinja environment
-    env = Environment(loader=FileSystemLoader("."))
-
-    # Load and render JSON template
-    try:
-        template = env.get_template(config_file)
-        rendered_json = template.render(variables)
-    except TemplateNotFound:
-        print(f"Error: Config file '{config_file}' not found.")
-        return 1
-    except Exception as e:
-        print(f"Error rendering template '{config_file}': {e}")
-        return 1
-
-    # Convert rendered JSON string to dict
-    try:
-        config_dict = json.loads(rendered_json)
-    except json.JSONDecodeError as e:
-        print(f"Error parsing rendered JSON '{config_file}': {e}")
-        return 1
-
-    # Create API endpoint URL
-    endpoint = "wlan-ssids"
-    url = generate_url(api_endpoint=endpoint)
-
-    bulk_key = "wlan-ssid"
     
-    # Create Profile
-    print(f"\nCreating {endpoint} profile...\n")
-    create_profile = Profiles.create_profile(
-        bulk_key=bulk_key,
-        path=url,
+    # Create and assign SSID Profile
+
+    create_profile(
+        "wlan-ssids",
+        "wlan-ssid",
+        variables["ssid"]["name"],
+        "wlan-ssids.json",
         central_conn=central_conn,
-        config_dict=config_dict,
+        env=env,
+        variables=variables,
+        device_persona=device_persona,
+        scope_type=scope_type,
+        scope_name=scope_name
     )
 
-    print(create_profile)
+    # Create and assign NTP Profile
 
-    # Assign Profile to Scope
-    scopes = Scopes(central_conn)
-    result = scopes.assign_profile_to_scope(
-        profile_name = f"{endpoint}/{config_dict['ssid']}",
-        profile_persona=device_persona,
-        scope=scope_type,
-        scope_name=scope_name,
+    create_profile(
+        "ntp",
+        "profile",
+        variables["ntp"]["name"],
+        "ntp.json",
+        central_conn=central_conn,
+        env=env,
+        variables=variables,
+        device_persona=device_persona,
+        scope_type=scope_type,
+        scope_name=scope_name
     )
-
-    # Check the result and print appropriate messages
-    if result:
-        print(f"\nSuccessfully assigned profile to {scope_name}\n")
-    else:
-        print(f"\nError in assigning profile\n")
-        exit()
 
 if __name__ == "__main__":
     exit(main())
